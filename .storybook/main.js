e041beac50f78bb42b86fc32a24f34c5797cca7c
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-11-24 12:16:05 +0100

    Install the SCSS preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/preset-scss',
     /*"@storybook/preset-create-react-app"*/
   ],
   core: {
